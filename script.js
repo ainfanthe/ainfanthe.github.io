b05c6774b2671f2b4a36bca2b8dba7a7a99b5b44
Andrés Hernández <105471058+ainfanthe@users.noreply.github.com>
2022-07-19 20:43:40 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var typed = new Typed('.typed', {
+	strings: [
+		'<b class="title"> Webpage</b>',
+		'<b class="title">Curriculum</b>'
+	],
+	stringsElement: '#cadenas-texto',
+	typeSpeed: 75,
+	startDelay: 100,
+	backSpeed: 75,
+	smartBackspace: true,
+	shuffle: false,
+	backDelay: 1500,
+	loop: true,
+	loopCount: false,
+	showCursor: true,
+	cursorChar: '|',
+	contentType: 'html',
+});
