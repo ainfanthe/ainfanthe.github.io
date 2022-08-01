55cb546b8553293f50db1901498ec1c4ef8ee3a8
Andrés Hernández <105471058+ainfanthe@users.noreply.github.com>
2022-08-01 09:50:43 -0500

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.btn');
-const articlesContainer = document.querySelector('.articles');
-
-toggleBtn.addEventListener('click', () => {
-  document.documentElement.classList.toggle('dark-theme');
-});
